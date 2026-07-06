fe413a6e6b332eaa855b0e8eb28c675d54c6cc89
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-06 18:59:27 +0530

    call and call back

--- recursion/question.java
+++ recursion/question.java
@@ -1,5 +1,12 @@
 public class question {
     public static void main(String[] args) {
-        System.out.println("udayan singh");
+       print(0,5);
+    }
+
+    public static void print(int x ,int n){
+        if(x > n) return;
+        System.out.println(x);
+        print(x+1,n);
     }
 }
+1:07
