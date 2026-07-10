ee16bd243657156f999ada155b532fe610f0fb65
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-10 16:45:57 +0530

    move all zero in the end

---
+++ array/question_03.java
@@ -0,0 +1,25 @@
+// move all zero in the end
+
+package array;
+
+public class question_03 {
+    public static void main(String[] args) {
+        int[] arr = {1,5,0,4,2,0,8};
+        int i = 0;
+        int j = arr.length-1;
+        while(i<j){
+          if(arr[i] == 0){
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+           j--;
+          }
+          i++;
+        
+        }
+        for(int ele : arr){
+            System.out.print(ele + " ");
+        }
+    }
+}
