228367a381608c2d09da2fe9f23847229e1e309c
udayan-kumar <udayanmannusingh01@gmail.com>
2026-07-19 07:00:24 +0530

    happy number

--- linked_list/singly_ll.java
+++ linked_list/singly_ll.java
@@ -165,6 +165,29 @@ public class singly_ll {
         return 0;
     }
 
+    boolean happy_number(int n){
+        int slow = n;
+        int fast = n;
+
+        do{
+            slow = findsquare(slow);
+            fast = findsquare(findsquare(fast));
+        }while(slow!=fast);
+
+        if(slow == 1) return true;
+        return false;
+
+    }
+    private int findsquare(int number){
+        int ans = 0 ;
+        while(number>0){
+            int rem = number % 10;
+            ans += rem * rem;
+            number /= 10;
+        }
+        return ans;
+    }
+
     void display(){
         Node temp = head;
         while(temp!=null){
